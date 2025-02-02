4077f31c9e46704cce6de556ffde1a52c606d24e
Muggle <863457210@qq.com>
2025-02-02 22:18:50 +0800

    基础框架构建

---
+++ packages/chat-ui/src/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { NConfigProvider } from 'naive-ui'
+</script>
+
+<template>
+  <NConfigProvider
+    class="h-full"
+    :theme="theme"
+    :theme-overrides="themeOverrides"
+    :locale="language"
+  >
+    <NaiveProvider>
+      <!-- <RouterView /> -->
+      <div>111</div>
+    </NaiveProvider>
+  </NConfigProvider>
+</template>
+
+<style scoped>
+
+</style>
